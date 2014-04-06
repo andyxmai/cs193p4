be1fbe1be936ba400ca9dc7363cc7924fe98e369
Andy Mai <andymai@rescomp-13-213410.stanford.edu>
2014-04-05 19:44:39 -0700

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Andy Mai on 4/5/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
