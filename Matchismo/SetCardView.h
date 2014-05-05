55ddd364c0443e063a53b67c8f44cc72389e011f
Naveen Krishnamurthi <naveen@rescomp-13-278068.stanford.edu>
2014-05-05 14:00:23 -0700

    Added shape drawing functions

---
+++ Matchismo/SetCardView.h
@@ -0,0 +1,15 @@
+//
+//  PlayingCardView.h
+//  Matchismo
+//
+//  Created by Andy Mai on 5/4/14.
+//  Copyright (c) 2014 CS193p. All rights reserved.
+//
+
+#import "CardView.h"
+
+@interface PlayingCardView : CardView
+@property (nonatomic, strong) NSString *suit;
+@property (nonatomic) NSUInteger rank;
+@property (nonatomic) BOOL faceUp;
+@end
